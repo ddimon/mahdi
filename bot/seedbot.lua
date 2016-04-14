a383567b06f2ed9002e55753e20508d0aea8cbfc
ddimon <dimon3star@gmail.com>
2016-04-14 13:46:29 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {192592723},--Sudo users
+    sudo_users = {142266345},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
